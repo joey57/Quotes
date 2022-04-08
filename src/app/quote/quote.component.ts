92542d6570039e004b8bc2ff986cd3d1431ab3ed
joey57 <njorogejoey5@gmail.com>
2022-04-08 15:23:36 +0300

    added date to ts

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
   title = 'Wise Words Quotes';
   quotes: Quote[]=[
-    new Quote(1,'Charles','Human spirit','The human spirit must prevail over technology.','Albert Einstein'),
-    new Quote (2,'Wendy','Technology…','Technology… the knack of so arranging the world that we dont have to experience it.','Max Frisch'),
-    new Quote (3,'Michael','The great myth','The great myth of our times is that technology is communication.','Libby Larsen'),
-    new Quote(4,'Allan','Communications tools','Communications tools dont get socially interesting until they get technologically boring.','Clay Shirky'),
-    new Quote(5,'Theodore','Programs and people','Programs must be written for people to read, and only incidentally for machines to execute.','Harold Abelson'),
-    new Quote(6,'Fallon','Technology and life','For a list of all the ways technology has failed to improve the quality of life, please press three.','Alice Kahn'),
+    new Quote(1,'Charles','Human spirit','The human spirit must prevail over technology.','Albert Einstein', new Date(2019,8,12)),
+    new Quote (2,'Wendy','Technology…','Technology… the knack of so arranging the world that we dont have to experience it.','Max Frisch', new Date(2019,7,14)),
+    new Quote (3,'Michael','The great myth','The great myth of our times is that technology is communication.','Libby Larsen',new Date(2020,6,3)),
+    new Quote(4,'Allan','Communication','Communications tools dont get socially interesting until they get technologically boring.','Clay Shirky',new Date(2020,8,7)),
+    new Quote(5,'Theodore','Code','Programs must be written for people to read, and only incidentally for machines to execute.','Harold Abelson',new Date(2021,8,17)),
+    new Quote(6,'Fallon','Tech','For a list of all the ways technology has failed to improve the quality of life, please press three.','Alice Kahn', new Date(2022,7,15)),
   ];
 
   quoteDelete(isRead, index){

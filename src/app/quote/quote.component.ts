00d3d50c85cf5111142a350c04fac0a2f5ca59aa
joey57 <njorogejoey5@gmail.com>
2022-04-09 11:07:52 +0300

    addedd highlight directive

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,8 +9,8 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
   title = 'Wise Quotes';
   quotes: Quote[]=[
-    new Quote(1,'Denis','Graditude',`If you look at what you have in life, you'll always have more. IF you look at what you don't have you'll anever have enough.`,'Oprah Winfrey', new Date(2022,8,12),0,0),
-    new Quote (2,'Wendy','Curiousity',`I have no special talents, I am only passionately curious.`,'Albert Einstein', new Date(2022,7,14),0,0),
+    new Quote(1,'Denis','Graditude',`If you look at what you have in life, you'll always have more. If you look at what you don't have you'll anever have enough.`,'Oprah Winfrey', new Date(2022,8,12),2,0),
+    new Quote (2,'Wendy','Curiousity',`I have no special talents, I am only passionately curious.`,'Albert Einstein', new Date(2022,7,14),1,0),
     new Quote (3,'Michael','Humanity',` What counts in life is not the mere fact that we have lived. It is what difference we have made to the lives of the others.`,'Nelson Mandela',new Date(2022,6,3),0,0),
     new Quote(4,'Allan','Leadership',`The very essence of leadership is that you have to have vision.You can't blow an uncertain trumpet.`,'Theodore M.Hesburgh',new Date(2022,8,7),0,0),
     new Quote(5,'Theo','Goals',`If you set your Goals ridiculously high and it's a failure, you will fail above everyone else's success.`,'James Cameron',new Date(2022,8,17),0,0),
